113d6f6e9f194f3f78b57f85412e94a0967af91a
Hatim Oudghiri <oudhatim07@gmail.com>
2026-07-10 22:15:10 -0400

    feat: setup automated testing pipeline with github actions

---
+++ tests/demo.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('Verify GitHub Actions demo page title', async ({ page }) => {
+    // 1. Instruct the browser to navigate to the Playwright website
+    await page.goto('https://playwright.dev/');
+
+    // 2. Assert that the title contains the word "Playwright"
+    await expect(page).toHaveTitle(/Playwright/);
+});
